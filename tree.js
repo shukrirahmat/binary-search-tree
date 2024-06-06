163feb2c5efedc121bae686dc1838a4b5ad4d7b6
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-06-06 19:47:30 +0800

    implemented levelOrder function

--- tree.js
+++ tree.js
@@ -2,10 +2,6 @@ function Tree(array) {
   const sortedarray = mergeSort([...new Set(array)]);
   let root = builtTree(sortedarray);
 
-  function getRoot() {
-    return root;
-  }
-
   function builtTree(array) {
     if (array.length === 0) return null;
 
@@ -38,7 +34,6 @@ function Tree(array) {
 
   function deleteItem(value) {
     root = deleteRec(value, root);
-    view();
   }
 
   function deleteRec(value, root) {
@@ -48,7 +43,7 @@ function Tree(array) {
       else if (root.left == null) return root.right;
       else if (root.right == null) return root.left;
       else {
-        const dataCopy = getNextClosest(root)
+        const dataCopy = getNextClosest(root);
         const rightRoot = deleteRec(dataCopy, root.right);
         root.data = dataCopy;
         root.right = rightRoot;
@@ -73,16 +68,73 @@ function Tree(array) {
     return currentNode.data;
   }
 
+  function find(value) {
+    return findRec(value, root);
+  }
+
+  function findRec(value, root) {
+    if (root == null) return null;
+    if (root.data === value) return root;
+
+    if (value < root.data) {
+      return findRec(value, root.left);
+    } else {
+      return findRec(value, root.right);
+    }
+  }
+
+  function levelOrder(callback = null) {
+    const queue = [root];
+    const values = [];
+
+    while (queue.length > 0) {
+      values.push(queue[0].data);
+      if (queue[0].left !== null) queue.push(queue[0].left);
+      if (queue[0].right !== null) queue.push(queue[0].right);
+      queue.shift();
+    }
+
+    if (callback !== null) values.forEach(callback);
+    else return values;
+  }
+
+  function levelOrderAlt(callback = null) {
+    const values = levelOrderRec([], [root]);
+
+    if (callback !== null) values.forEach(callback);
+    else return values;
+  }
+
+  function levelOrderRec(values, queue) {
+    if (queue.length === 0) {
+      return values;
+    }
+
+    values.push(queue[0].data);
+    if (queue[0].left !== null) queue.push(queue[0].left);
+    if (queue[0].right !== null) queue.push(queue[0].right);
+    queue.shift();
+
+    return levelOrderRec(values, queue);
+  }
+
   function view() {
-    prettyPrint(getRoot());
+    prettyPrint(root);
   }
 
   return {
     view,
     insert,
     deleteItem,
+    find,
+    levelOrder,
+    levelOrderAlt
   };
 }
 
 const t = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 t.view();
+
+function addOne(x) {
+  console.log(x + 1);
+}
